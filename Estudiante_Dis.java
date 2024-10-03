4c6d7438acf1b0731ea59331478353172307c5da
SrAlejo4 <alejandrogarci087@gmail.com>
2024-10-02 22:23:39 -0500

    Prueba 2

--- Estudiante_Dis.java
+++ Estudiante_Dis.java
@@ -1,4 +1,56 @@
 package Trabajo_Final;
 
 public class Estudiante_Dis {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String modalidad;
+    private int cantMaterias;
+    private int serial;
+    
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getModalidad() {
+        return modalidad;
+    }
+    public void setModalidad(String modalidad) {
+        this.modalidad = modalidad;
+    }
+    public int getCantMaterias() {
+        return cantMaterias;
+    }
+    public void setCantMaterias(int cantMaterias) {
+        this.cantMaterias = cantMaterias;
+    }
+    public int getSerial() {
+        return serial;
+    }
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+
+    
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 package Trabajo_Final;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("PRIMERA PRUEBA DE NO ESTALLE");
+    }
 }
